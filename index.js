fefe9cd247fff9491594eb40c8396b9f8a487ce8
rangsiman <rangsimanp58@email.nu.ac.th>
2018-10-12 09:03:07 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 8000
+
+//app.use(express.static('public'))
+
+app.use('/static', express.static(path.join(__dirname, 'css')))
+app.get('/',(req, res) => res.sendFile(path.join(__dirname, 'resume.html')))
+
+app.listen(port, () => console.log('app listening on port'+port))
+
+
