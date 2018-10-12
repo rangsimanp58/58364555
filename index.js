2a6a30aaed993ea15f2ee14a70a045d17e5369e6
rangsimanp58 <43905275+rangsimanp58@users.noreply.github.com>
2018-10-12 17:38:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express()
 const port = 8000;
 
 //app.use(express.static('public'))
-app.use('/static', express.static(path.join(__dirname, 'css')))
-app.get('/',(req, res) => res.sendFile(path.join(__dirname, 'resume.html')))
+app.use('/static', express.static(path.join(__dirname, 'public')))
+app.get('/',(req, res) => res.sendFile(path.join(__dirname, 'index.html')))
 
 app.listen(port, () => console.log('app listening on port'+port))
 
